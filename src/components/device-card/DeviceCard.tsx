3488c9011cff9cbb949e91f42e83b4fddae2c7a7
Josh Choo <dev@joshuous.com>
2020-04-24 13:31:53 +0800

    DeviceCard: apply initial styling for mobile devices

--- src/components/device-card/DeviceCard.tsx
+++ src/components/device-card/DeviceCard.tsx
@@ -22,54 +22,54 @@ export const DeviceCard: React.FC<{ deviceDetails: ResultAll }> = ({
   const date = faunaTimestampToDate(_ts);
 
   return (
-    <div className="device-card">
-      <div className="device-card__header">
-        <div className="device-id">{`ID: ${_id}`}</div>
-        <div className="result-datetime">
+    <div className="bg-white">
+      <div className="flex flex-row justify-between px-4 py-2 bg-gray-900 text-gray-100">
+        <div className="text-sm">{`ID: ${_id}`}</div>
+        <div className="text-sm">
           {`${formatDateTime(
             date
           )} - ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`}
         </div>
       </div>
       <div className="device-card__body">
-        <div className="device-card__primary-details">
-          <div className="device-name">
-            <div className="device-name__label">Name</div>
-            <div className="device-name__value">{device_name}</div>
+        <div className="flex flex-row flex-wrap justify-around p-4 text-center">
+          <div className="flex flex-col px-4 py-2">
+            <div className="text-sm">Name</div>
+            <div className="text-lg">{device_name}</div>
           </div>
-          <div className="device-model">
-            <div className="device-model__label">Model</div>
-            <div className="device-model__value">{device_model}</div>
+          <div className="flex flex-col px-4 py-2">
+            <div className="text-sm">Model</div>
+            <div className="text-lg">{device_model}</div>
           </div>
-          <div className="device-board">
-            <div className="device-board__label">Board</div>
-            <div className="device-board__value">{device_board}</div>
+          <div className="flex flex-col px-4 py-2">
+            <div className="text-sm">Board</div>
+            <div className="text-lg">{device_board}</div>
           </div>
-          <div className="device-hardware">
-            <div className="device-hardware__label">Hardware</div>
-            <div className="device-hardware__value">{device_hardware}</div>
+          <div className="flex flex-col px-4 py-2">
+            <div className="text-sm">Hardware</div>
+            <div className="text-lg">{device_hardware}</div>
           </div>
-          <div className="android-version">
-            <div className="android-version__label">Android</div>
-            <div className="android-version__value">{android_version}</div>
+          <div className="flex flex-col px-4 py-2">
+            <div className="text-sm">Android</div>
+            <div className="text-lg">{android_version}</div>
           </div>
         </div>
-        <div className="device-card__secondary-details">
-          <div className="kernel-version">
-            <div className="kernel-version__label">Kernel Version</div>
-            <div className="kernel-version__value">{kernel_version}</div>
+        <div className="flex flex-col px-4">
+          <div className="flex flex-col py-2">
+            <div className="font-bold text-sm">Kernel Version</div>
+            <div className="text-sm">{kernel_version}</div>
           </div>
-          <div className="fingerprint">
-            <div className="fingerprint__label">Fingerprint</div>
-            <div className="fingerprint__value">{fingerprint}</div>
+          <div className="flex flex-col py-2">
+            <div className="font-bold text-sm">Fingerprint</div>
+            <div className="text-sm">{fingerprint}</div>
           </div>
-          <div className="build-type">
-            <div className="build-type__label">Built Type</div>
-            <div className="build-type__value">{build_type}</div>
+          <div className="flex flex-col py-2">
+            <div className="font-bold text-sm">Build Type</div>
+            <div className="text-sm">{build_type}</div>
           </div>
-          <div className="build-time">
-            <div className="built-time__label">Built Time</div>
-            <div className="build-time__value">{build_time}</div>
+          <div className="flex flex-col py-2">
+            <div className="font-bold text-sm">Build Time</div>
+            <div className="text-sm">{build_time}</div>
           </div>
         </div>
       </div>
