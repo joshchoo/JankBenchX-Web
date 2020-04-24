974c1599f6ea2c511f02de79b69f7562280f2596
Josh Choo <dev@joshuous.com>
2020-04-24 09:33:12 +0800

    more style changes to results page

--- src/components/result-tile/ResultTile.tsx
+++ src/components/result-tile/ResultTile.tsx
@@ -22,19 +22,16 @@ const shortenKernelText = (kernelVersion: string) => {
 
 export const ResultTile: React.FC<{ result: Result }> = ({ result }) => {
   return (
-    <div className="shadow-lg mx-auto mt-5 mb-5 rounded-md bg-white max-w-sm sm:max-w-lg">
-      <div className="flex justify-between items-center px-4 py-2 rounded-t-lg bg-gray-200">
-        <div className="sm:text-lg">{`ID: ${result._id}`}</div>
-        <div className="sm:text-lg">
-          {formatDateTime(faunaTimestampToDate(result._ts))}
+    <div className="shadow-xl mx-auto mt-5 mb-5 rounded bg-white max-w-sm sm:max-w-lg">
+      <div className="">
+        <div className="p-4 text-center rounded-t bg-gray-900 text-gray-100">
+          <div className="text-2xl font-medium sm:text-2xl">{`${result.device_name} - ${result.device_model}`}</div>
+          <div className="text-gray-100 text-sm">
+            {shortenKernelText(result.kernel_version)}
+          </div>
         </div>
-      </div>
-      <div className="p-2">
-        <div className="text-center py-6 text-2xl font-medium sm:text-3xl">{`${result.device_name} - ${result.device_model}`}</div>
-        <div className="hidden text-center text-base text-lg font-light pt-1 pb-5 sm:block">
-          {shortenKernelText(result.kernel_version)}
-        </div>
-        <div className="pb-1">
+        <hr className="" />
+        <div className="p-2">
           <table className="w-full">
             <thead>
               <tr className="result__table-header">
@@ -69,6 +66,12 @@ export const ResultTile: React.FC<{ result: Result }> = ({ result }) => {
               })}
             </tbody>
           </table>
+        </div>
+      </div>
+      <div className="flex justify-between items-center px-4 py-2 rounded-b text-sm bg-gray-900 text-gray-100">
+        <div className="">{`ID: ${result._id}`}</div>
+        <div className="">
+          {formatDateTime(faunaTimestampToDate(result._ts))}
         </div>
       </div>
     </div>
