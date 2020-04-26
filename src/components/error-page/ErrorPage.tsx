266ae489aa04a4805854e7fc4c0cdf97882129f7
Josh Choo <dev@joshuous.com>
2020-04-26 10:51:02 +0800

    improve ErrorPage styling and add data-testid

--- src/components/error-page/ErrorPage.tsx
+++ src/components/error-page/ErrorPage.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export const ErrorPage: React.FC = () => {
-  return <div>Error</div>;
+  return (
+    <div
+      data-testid="error-page"
+      className="flex flex-col justify-center items-center"
+      style={{ height: '60vh' }}
+    >
+      <div
+        className="bg-cover"
+        style={{
+          backgroundImage: `url("https://i.imgur.com/yW2W9SC.png)`,
+          width: '40vh',
+          height: '40vh',
+        }}
+      />
+      <div className="pt-8 text-3xl">An error occurred :(</div>
+    </div>
+  );
 };
