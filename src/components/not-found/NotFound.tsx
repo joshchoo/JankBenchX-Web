7bdede236e593d15113a75bb03947a645d377357
Josh Choo <dev@joshuous.com>
2020-04-26 10:49:12 +0800

    improve NotFound styling

--- src/components/not-found/NotFound.tsx
+++ src/components/not-found/NotFound.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export const NotFound: React.FC = () => {
-  return <div data-testid="not-found">Not Found</div>;
+  return (
+    <div
+      data-testid="not-found"
+      className="flex flex-col justify-center items-center"
+      style={{ height: '60vh' }}
+    >
+      <div
+        className="bg-cover"
+        style={{
+          backgroundImage: `url("https://i.imgur.com/Q2BAOd2.png)`,
+          width: '40vh',
+          height: '40vh',
+        }}
+      />
+      <div className="pt-8 text-3xl">Not Found</div>
+    </div>
+  );
 };
