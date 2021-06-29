e4f54ed477430fd6f44d4103dc0636bf149db119
Josh Choo <dev@joshuous.com>
2021-06-30 01:21:28 +0800

    Add fix for Recharts breakage on Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh(), rechartsPlugin()],
+});
+
+// Temporary fix for https://github.com/vitejs/vite/issues/3477
+function rechartsPlugin() {
+  return {
+    name: "recharts_plugin",
+    transform(code: string, id: string) {
+      if (id.includes("reduce-css-calc/dist/parser.js")) {
+        return code.replace(`typeof require !== 'undefined'`, "true");
+      }
+
+      return null;
+    },
+  };
+}
